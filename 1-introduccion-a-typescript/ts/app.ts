1f6719d0f1a95c05eca1802923b69ce28f22903f
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-04-11 08:38:32 -0400

    upd

--- 1-introduccion-a-typescript/ts/app.ts
+++ 1-introduccion-a-typescript/ts/app.ts
@@ -7,30 +7,47 @@
     const existe:boolean = false;
   
     // Tuplas
-    const parejaHeroes:[batman,superman];
-    const villano = ['Lex Lutor',5,true];
+    const parejaHeroes:[string, string] = [batman,superman];
+    const villano:[string, number, boolean] = ['Lex Lutor',5,true];
+
+    console.log(parejaHeroes);
   
     // Arreglos
-    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliados:Array<string> = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliadoss:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+
+    aliados.forEach(v => console.log(v.toUpperCase()));
+    aliadoss.forEach(v => console.log(v));
   
     //Enumeraciones
-    const fuerzaFlash = 5;
-    const fuerzaSuperman = 100;
-    const fuerzaBatman = 1;
-    const fuerzaAcuaman = 0;
+    enum fuerza{
+      fuerzaAcuaman, 
+      fuerzaBatman,
+      fuerzaFlash = 5,
+      fuerzaSuperman = 100,
+    }
+
+    const fuerzaFlash:fuerza = fuerza.fuerzaFlash;
+    const fuerzaSuperman:fuerza = fuerza.fuerzaSuperman;
+    const fuerzaBatman:fuerza = fuerza.fuerzaBatman;
+    const fuerzaAcuaman:fuerza = fuerza.fuerzaAcuaman;
   
     // Retorno de funciones
-    function activar_batiseñal(){
+    function activar_batiseñal():string{
       return 'activada';
     }
+
+    activar_batiseñal();
   
-    function pedir_ayuda(){
+    function pedir_ayuda():void{
       console.log('Auxilio!!!');
     }
+
+    pedir_ayuda();
   
     // Aserciones de Tipo
     const poder: any = '100';
-    const largoDelPoder:number = poder.length;
+    const largoDelPoder:number = (poder as string).length
     console.log( largoDelPoder );
   
   
---
+++ 1-introduccion-a-typescript/ts/tipos/as.ts
@@ -0,0 +1,13 @@
+(() => {
+
+    enum enumeracion {
+        a=10,
+        b,
+        c=9,
+        d
+      }
+
+      let a:enumeracion = enumeracion.d;
+      console.log(a);
+
+})()
